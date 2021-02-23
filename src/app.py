6fc24279152444b70be75051dea3b6a9c6fb0eaf
Jonathan Villalobos Mora <hackertristania@gmail.com>
2021-02-23 21:58:01 +0000

    exer 07

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify, request,json
+from flask import Flask, jsonify, request, json
 app = Flask(__name__)
 
 todos = [{"label":"My first task","done":False}]
 
 @app.route('/todos', methods=['GET'])
-def hello_world():
+def get_todo():
 
     return jsonify(todos)
 
@@ -14,18 +14,15 @@ def add_new_todo():
     decoded_object = json.loads(request_body)
     todos.append(decoded_object)
     json_text = jsonify(todos)
+    
     return json_text 
 
+
+        
 # These two lines should always be at the end of your app.py file.
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
 
-
-# request_body = request.data
-#     decoded_object = json.loads(request_body)
-#     todos.append(decoded_object)
-#     json_text = jsonify(todos)
-#     return json_text 
 
 # @app.route('/todos', methods=['POST'])
 # def add_new_todo():
